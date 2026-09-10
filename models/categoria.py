c2b8f9a8441aa882e5d5e9314d31f0967ae129a4
CristianPonce2006 <alexisrtz06@gmail.com>
2026-09-10 10:16:40 -0600

    modelo de categoriasCreate, Categoria y CategoriaUpdate terminado

--- models/categoria.py
+++ models/categoria.py
@@ -1,9 +1,11 @@
-import sqlmodel import SQLModel, Field
-from typing import Optional
+from sqlmodel import SQLModel, Field
 
 class CategoriaBase(SQLModel):
     nombre: str = Field(nullable=False, max_length=100, min_length=3)
-    
+
+class Categoria(CategoriaBase):
+    id: int|None = Field(default=None) 
+
 class CategoriaCreate(CategoriaBase):
     pass
 class CategoriaUpdate(CategoriaBase):
