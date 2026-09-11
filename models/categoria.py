505dfc47e0cd18e18ad2364c0c8fc4283515cf45
CristianPonce2006 <alexisrtz06@gmail.com>
2026-09-11 17:34:47 -0600

    modelo de categorias creado

--- models/categoria.py
+++ models/categoria.py
@@ -3,8 +3,9 @@ from sqlmodel import SQLModel, Field
 class CategoriaBase(SQLModel):
     nombre: str = Field(nullable=False, max_length=100, min_length=3)
 
-class Categoria(CategoriaBase):
-    id: int|None = Field(default=None) 
+class Categoria(CategoriaBase, table=True):
+    __tablename__ = "categorias" #type: ignore
+    id: int|None = Field(default=None, primary_key=True) 
 
 class CategoriaCreate(CategoriaBase):
     pass
---
+++ routers/categoria_router.py
@@ -0,0 +1,20 @@
+from fastapi import APIRouter, HTTPException, status, Query
+from models.categoria import Categoria, CategoriaCreate
+from sqlmodel import select
+from config.session_dependencia import SessionDeDependencia
+
+router = APIRouter()
+
+@router.get("categorias", response_model=list[Categoria], status_code=status.HTTP_200_OK)
+async def get_categorias(session: SessionDeDependencia, offset: int=Query(0, ge=0), limit: int=Query(20, ge=1)):
+    consulta = select(Categoria).offset(offset).limit(limit)
+    resultado_consulta = session.exec(consulta)
+    return resultado_consulta.all()
+
+@router.get("/categorias/{id}", response_model=Categoria, status_code=status.HTTP_200_OK)
+async def get_categoria(id: int,session: SessionDeDependencia):
+    consulta = select(Categoria).where(Categoria.id == id)
+    resultado_consulta = session.exec(consulta)
+    if not resultado_consulta:
+        raise HTTPException(status_code=404, detail="Categoria no encontrada")
+    return resultado_consulta
