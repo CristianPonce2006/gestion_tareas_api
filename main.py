30b309cb0e4bb13376f396dacf471658fa6268db
CristianPonce2006 <alexisrtz06@gmail.com>
2026-09-14 19:34:17 -0600

    configuracion de cors

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI, status
+from fastapi.middleware.cors import CORSMiddleware
 from contextlib import asynccontextmanager
 from config.db import crear_db_y_tablas
 from routers.categoria_router import router as categorias_router
@@ -14,7 +15,19 @@ async def lifespan(app: FastAPI):
 app = FastAPI(lifespan=lifespan)
 app.title = "API Tienda X"
 app.version = "0.0.1"
+origins = [
+    "http://localhost",
+    "http://localhost:9000", # Cambia esto por el puerto exacto de tu app Flutter
+    # "*", # Puedes usar un asterisco en desarrollo para permitir cualquier origen
+]
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"], # Permite todos los métodos (GET, POST, PUT, DELETE, etc.)
+    allow_headers=["*"], # Permite todos los headers
+)
 @app.get("/", summary="Comprobando Api", status_code=status.HTTP_200_OK)
 async def home():
     return {"message": "ok"}
