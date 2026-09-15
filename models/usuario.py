001fbdc8a3583ee0f201a65dd68542b033901076
CristianPonce2006 <alexisrtz06@gmail.com>
2026-09-14 19:33:04 -0600

    agregue validacion para el email

--- models/usuario.py
+++ models/usuario.py
@@ -2,7 +2,7 @@ from sqlmodel import SQLModel, Field
 from pydantic import EmailStr
 class UsuarioBase(SQLModel):
     nombre: str = Field(nullable=False, max_length=100, min_length=3)
-    email: str = Field(nullable=False, max_length=150, min_length=3)
+    email: EmailStr = Field(nullable=False, max_length=150, min_length=3)
 class Usuario(UsuarioBase, table=True):
     __tablename__ = "usuarios" #type: ignore
     id: int|None = Field(default=None, primary_key=True) 
