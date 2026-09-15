62aa20034678b904d8a313785a774bcd2af88c2b
CristianPonce2006 <alexisrtz06@gmail.com>
2026-09-14 19:33:25 -0600

    cambio de nombre de los metodos

--- routers/tareas_router.py
+++ routers/tareas_router.py
@@ -6,13 +6,13 @@ from config.session_dependencia import SessionDeDependencia
 router = APIRouter()
 
 @router.get("/tareas", response_model=list[Tarea], status_code=status.HTTP_200_OK)
-async def get_Tareas(session: SessionDeDependencia, offset: int = Query(0, ge=0), limit: int =Query(20, ge=1)):
+async def get_tareas(session: SessionDeDependencia, offset: int = Query(0, ge=0), limit: int =Query(20, ge=1)):
     consulta = select(Tarea).offset(offset).limit(limit)
     resultado_consulta = session.exec(consulta)
     return resultado_consulta.all()
 
 @router.get("/tareas/{id}", response_model=Tarea, status_code=status.HTTP_200_OK)
-async def get_Tarea(id: int,session: SessionDeDependencia):
+async def get_tarea(id: int,session: SessionDeDependencia):
     consulta = select(Tarea).where(Tarea.id == id)
     resultado_consulta = session.exec(consulta).first()
     if not resultado_consulta:
