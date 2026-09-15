305eb377a89a5e9b497fdc33662c5e8371ac33be
CristianPonce2006 <alexisrtz06@gmail.com>
2026-09-14 20:52:14 -0600

    verbo post probado con validaciones para id

--- routers/tareas_router.py
+++ routers/tareas_router.py
@@ -1,5 +1,7 @@
 from fastapi import APIRouter, HTTPException, status, Query
 from models.tarea import Tarea, TareaCreate
+from models.usuario import Usuario
+from models.categoria import Categoria
 from sqlmodel import select
 from config.session_dependencia import SessionDeDependencia
 
@@ -21,6 +23,22 @@ async def get_tarea(id: int,session: SessionDeDependencia):
 
 @router.post("/tareas", response_model=Tarea, status_code=status.HTTP_201_CREATED)
 async def create_tarea(session: SessionDeDependencia, data: TareaCreate):
+    #validamos si existe la categoría
+    consulta_categoria = select(Categoria).where(
+        Categoria.id == data.categoria_id
+    )
+    categoria = session.exec(consulta_categoria).first()
+    if not categoria:
+        raise HTTPException(status_code=404, detail=f"Categoría con id: {data.categoria_id} no encontrada")
+    
+    #validamos si existe el usuario
+    consulta_usuario = select(Usuario).where(
+    Usuario.id == data.usuario_id
+    )
+    usuario = session.exec(consulta_usuario).first()
+    if not usuario:
+        raise HTTPException(status_code=404, detail=f"Usuario no encontrado")
+    
     tarea_nueva = Tarea(
         usuario_id=data.usuario_id,
         categoria_id=data.categoria_id,
